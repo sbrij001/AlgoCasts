679b579535c889ed400f0ba9333c56625f4857d1
sbrij001 <sbrij001@yahoo.com>
2019-08-12 12:09:00 -0400

    anagrams time complex O(logn)

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,31 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// first check the length of the string
+// no spaces or punctuation
+// turn everything to lowercase
+// function should return true or false
+
+//1. take str a and b and rid them of spaces and punctuation.
+//2. set all the characters of the new string to lowercase.
+//3. check the length of both strings and make sure they are equal
+//   if not return false
+//4. use the .include helper method to check if the chars in str a
+//   are the same as str b.
+//5. return true or false based on step 3
+function anagrams(stringA, stringB) {
+  let newStringA = stringA.toLowerCase().replace(/[ .,\/#!$%\^&\*;:{}=\-_`~()]/g,"").split("").sort().join("");
+  let newStringB = stringB.toLowerCase().replace(/[ .,\/#!$%\^&\*;:{}=\-_`~()]/g,"").split("").sort().join("");
+
+  console.log(newStringA)
+  console.log(newStringB)
+
+  if (newStringA.length !== newStringB.length) {
+    return false
+  }
+
+  return newStringA === newStringB;
+
+}
 
 module.exports = anagrams;
