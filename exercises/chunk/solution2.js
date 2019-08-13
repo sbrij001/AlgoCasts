16594b5de81747652e96774d258cee50aa377027
sbrij001 <sbrij001@yahoo.com>
2019-08-13 16:37:54 -0400

    set up the properties data and next for the node class in linked lists

---
+++ exercises/chunk/solution2.js
@@ -0,0 +1,29 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+
+// 1. create an empty chunked arr
+// 2. create an index start at 0
+// 3. while index is less than array.length
+//    push a slice of length size from array into chunked.
+//    add size to index.
+
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+
+  while(index < array.length){
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+  return chunked
+};
+
+module.exports = chunk;
