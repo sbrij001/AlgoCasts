15b4b5e901c0dc3f832d8314d749e26c99310377
sbrij001 <sbrij001@yahoo.com>
2019-08-13 14:14:29 -0400

    first solution for capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,22 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+
+// take in the arg of a string
+// return a capitalized version of this string
+// each word in the sent should be capitalized
+
+// 1. split the string into an arr
+// 2. map thru the arr and capitalize each word in the array
+//    use the slice method to break apart word 
+// 3. join array to turn it back into a string.
+// 4. return newly capitalized string.
+
+function capitalize(str) {
+  let arrOfWords = str.split(" ").map((word) => {
+    return word[0].toUpperCase() + word.slice(1)
+  })
+  return arrOfWords.join(" ");
+}
 
 module.exports = capitalize;
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,23 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// given a num and func must return int
+// when reversing numbers containing 0's
+// one must get rid of 0s
+// take into account negatives
+
+// turn the number into a string and arr
+// reverse the arr
+// check if the num is pos or neg
+// if neg make sure to add the - at index 0
+// use .replace to get rid of 0s
+
+
+function reverseInt(n) {
+  let reversed = n.toString().split("").reverse().join("");
+
+  return parseInt(reversed) * Math.sign(n)
+};
+
 
 module.exports = reverseInt;
