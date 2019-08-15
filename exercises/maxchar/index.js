f1bdfd4f5ee1c282a48111dc7c38db6b8bbaef70
sbrij001 <sbrij001@yahoo.com>
2019-08-15 18:33:41 -0400

    created a node with insert and contians method for BSTs

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,34 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+
+// 1. create a hash
+// 2. save all of the character into the hash
+// 3. if the char in the hash exists increment by 1
+// 4. else set the value to 1
+// 5. create a max var to save max num
+// 6. create a maxChar var to save the character with the max char
+// 5. loop thru the resulting hash saving the char and the value of the char in max
+function maxChar(str) {
+  const charHash = {};
+  let max = 0;
+  let maxChar = '';
+
+  for(i of str){
+    character = i
+    if(charHash[character]){
+      charHash[character]++
+    }else {
+      charHash[character] = 1
+    }
+  }
+  for(char in charHash){
+    if (charHash[char] > max) {
+      max = charHash[char]
+      maxChar = char
+    }
+  }
+  return maxChar;
+};
 
 module.exports = maxChar;
