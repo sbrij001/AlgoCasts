fe25dac6c933276bcdd36543bff58d1ed7122cc6
sbrij001 <sbrij001@yahoo.com>
2019-08-15 16:21:16 -0400

    memoize solution for recursive solution for fib

--- exercises/fib/recursiveSolution.js
+++ exercises/fib/recursiveSolution.js
@@ -1,10 +1,25 @@
 // solve fib using recursion
+// solve fib using a memoize function
+
+function memoize(fn){
+  //DECLARE A STORAGE AREA
+  const cache = {};
+  return function(...args){
+    if(cache[args]){
+      return cache[args];
+    }
+    const result = fn.apply(this, args)
+    cache[args] = result;
+
+    return result;
+  }
+};
 
 function fib(n){
-  // only return a number when n is less than 2 
+  // only return a number when n is less than 2
   if (n < 2){
     return n;
   }
-
   return fib(n - 1) + fib(n - 2);
-}
+};
+fib = memoize(fib);
