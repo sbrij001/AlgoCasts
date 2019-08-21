0b98faebdf95d5f6b9f21a2247e12dd9a9ec553d
sbrij001 <sbrij001@yahoo.com>
2019-08-21 14:29:08 -0400

    solution to findNum

---
+++ exercises/linearsearch/linearsearch.js
@@ -0,0 +1,13 @@
+// accepts an arg of an array full of nums and an arg of a value
+// loop thru the entire array
+// check if the current ele is equal to the value
+// if it is return the index at which it is found
+// if it is never found return -1
+
+function findNum(array, value){
+  for(let i = 0; - < array.length; i++){
+    if (array[i] === value) {
+      return i;
+    }
+  }
+};
