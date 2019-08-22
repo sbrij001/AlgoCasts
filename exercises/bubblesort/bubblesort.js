c50145c186968ec889b7834167937f01e5e32552
sbrij001 <sbrij001@yahoo.com>
2019-08-22 19:04:23 -0400

    bubble sort solution

---
+++ exercises/bubblesort/bubblesort.js
@@ -0,0 +1,22 @@
+// create a function called bubble sort that takes in an array
+// start looping with an var called i the end of the array towards the beg
+// start an inner loop with a var called j from the begining until i - 1
+// if array of j is greator than arr[j + 1] swap those two values.
+// return the sorted array
+
+function bubbleSort(arr){
+  const sorted = [];
+  let temp = 0;
+
+  for( let i = arr.length; i > 0; i-- ){
+    for(let j = 0; j < i - 1; i++){
+      if(arr[j] > arr[j + 1]){
+        //SWAP
+      let temp = arr[j]
+      arr[j] = arr[j + 1]
+      arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
