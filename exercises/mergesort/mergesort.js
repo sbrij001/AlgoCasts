d937c3a662de6af24a72053b5addfec88d8d1f3e
sbrij001 <sbrij001@yahoo.com>
2019-10-07 20:33:01 -0400

    created variables and base cases for if an array is empty

--- exercises/mergesort/mergesort.js
+++ exercises/mergesort/mergesort.js
@@ -6,4 +6,22 @@
 // push the values in to the results then move onto the next
 // 3. if the values in the first array is larger than the values in the second array
 // push the values of the second array into the results then move onto the next
-// 4. once we exhaust the array, push all the remaining values from the other array 
+// 4. once we exhaust the array, push all the remaining values from the other array
+
+function mergeArrays(arr1, arr2){
+  const merged = [];
+  let i = 0;
+  let j = 0;
+
+  // base cases
+  if (arr1.length === 0) {
+      return arr2;
+  }
+  if (arr1.length === 0) {
+      return arr2;
+  }
+
+  while (arr) {
+
+  }
+}
