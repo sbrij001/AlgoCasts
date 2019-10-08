1263bd2fcab1e02952349cb093c499d2b26e3a4b
sbrij001 <sbrij001@yahoo.com>
2019-10-07 20:23:19 -0400

    created pseudocode for merge function

---
+++ exercises/mergesort/mergesort.js
@@ -0,0 +1,9 @@
+// create a merger function that will merger two arrays into one
+
+// pseudocode
+// 1. create a merge array that will be returned
+// 2. if the values in the first array is smaller than the values in the second array
+// push the values in to the results then move onto the next
+// 3. if the values in the first array is larger than the values in the second array
+// push the values of the second array into the results then move onto the next
+// 4. once we exhaust the array, push all the remaining values from the other array 
