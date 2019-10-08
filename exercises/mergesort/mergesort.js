92d6b6396502c5c5bdad9faceec07c6e5a22c4a7
sbrij001 <sbrij001@yahoo.com>
2019-10-07 20:47:41 -0400

    created 2 extra while loops to push the result of the results from the leftover arry

--- exercises/mergesort/mergesort.js
+++ exercises/mergesort/mergesort.js
@@ -20,8 +20,23 @@ function mergeArrays(arr1, arr2){
   if (arr2.length === 0) {
       return arr1;
   }
-
-  while (arr) {
-
+  // loops both array at the same time
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      merged.push(arr1[i])
+      i++
+    }else {
+      merged.push(arr2[j])
+      j++
+    }
   }
+
+  while (i < arr1.length) {
+    merged.push(arr1[i])
+    i++
+  }
+  while (j < arr2.length) {
+    merged.push(arr2[j])
+  }
+  return results
 }
